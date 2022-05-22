ce1a79d242309cb3dbc47a75e32c15bcf43b32e7
kubamak <kubamachnik@gmail.com>
2022-05-22 11:49:23 +0200

    project initial data

---
+++ src/main/java/pl/km/exercise221/Exercise221Application.java
@@ -0,0 +1,13 @@
+package pl.km.exercise221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise221Application.class, args);
+    }
+
+}
